abae347e91154641d3bd51eeeef630390c9ba232
Paul Maxwell <maxwellpaulm@gmail.com>
2023-08-15 19:15:03 -0400

    Changing colorscheme

--- lua/user/colorscheme.lua
+++ lua/user/colorscheme.lua
@@ -1,10 +1,9 @@
-local colorscheme = "darkplus"
--- local colorscheme = "nightfox"
--- local colorscheme = "tokyonight"
+-- local colorscheme = "darkplus"
 -- local colorscheme = "carbonfox"
--- local colorscheme = "onedark"
--- local colorscheme = "nord"
--- local colorscheme = "material"
+
+local colorscheme = "onedark"
+require('onedark').setup {style = 'warmer'}
+require('onedark').load()
 
 local status_ok, _ = pcall(vim.cmd, "colorscheme " .. colorscheme)
 if not status_ok then
