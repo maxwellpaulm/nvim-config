ee807df6ab2f52dfc9b9cffedde6c5e06fe710b4
Paul Maxwell <maxwellpaulm@gmail.com>
2023-11-21 08:31:00 -0500

    Update install

--- install.sh
+++ install.sh
@@ -1,5 +1,13 @@
+# constants
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 USER_CONFIG_DIR=$HOME/.config
-rm -r $PROG_CONFIG_DIR
-ln -v -s $SCRIPT_DIR $USER_CONFIG_DIR/nvim
 
+# functions
+link_config () {
+    echo ""
+    PROG_CONFIG_DIR=$USER_CONFIG_DIR/$1
+    ln -v -s -f $SCRIPT_DIR $PROG_CONFIG_DIR
+    echo "Linked $1 config"
+}
+
+link_config "nvim"
